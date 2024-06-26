5d083d088e9c03ec243e6a5995c076ad786a5cb8
arshadakl <arshadayanikkal@gmail.com>
2024-06-26 09:19:57 +0530

    feat:cors added

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
 import configCloudinary from './utils/Cloudinary.js'
-
+import cors from "cors"
 import authRoutes from './routes/authRoutes.js';
 import blogRoutes from './routes/blogRoutes.js';
 import commentRoutes from './routes/commentRoutes.js';
@@ -14,7 +14,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 configCloudinary()
 // i want to add cors
-
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+}));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
